906d928e862980aef66a6da760736451b5c855d7
Takashi Kanemoto <ttskch@gmail.com>
2020-07-03 14:05:50 +0900

    refactor: 💡 twch: firestoreに移行したことでモデルクラスのjson表現は不要になったので削除

--- twch/lib/models/account.dart
+++ twch/lib/models/account.dart
@@ -1,15 +1,6 @@
-import 'dart:convert';
-
 class Account {
   final String id;
   final String username;
 
   Account({this.id, this.username});
-
-  Account.fromJsonString(String jsonString)
-      : this(username: json.decode(jsonString)['username']);
-
-  String toJsonString() {
-    return json.encode({'username': username});
-  }
 }
--- twch/lib/models/account_log.dart
+++ twch/lib/models/account_log.dart
@@ -1,19 +1,7 @@
-import 'dart:convert';
-
 class AccountLog {
   final String id;
   final int followerCount;
   final DateTime dateTime;
 
   AccountLog({this.id, this.followerCount, this.dateTime});
-
-  AccountLog.fromJsonString(String jsonString)
-      : this(
-            followerCount: json.decode(jsonString)['followerCount'],
-            dateTime: DateTime.parse(json.decode(jsonString)['dateTime']));
-
-  String toJsonString() {
-    return json
-        .encode({'followerCount': followerCount, 'date': dateTime.toString()});
-  }
 }

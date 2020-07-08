8304444389a760b6f2c280011fd4c80c01c1a854
Takashi Kanemoto <ttskch@gmail.com>
2020-07-08 19:07:39 +0900

    feat: 🎸 managing_state: パターンB

--- managing_state/lib/main.dart
+++ managing_state/lib/main.dart
@@ -14,27 +14,47 @@ class MyApp extends StatelessWidget {
           title: Text('Flutter Demo'),
         ),
         body: Center(
-          child: TapboxA(),
+          child: ParentWidget(),
         ),
       ),
     );
   }
 }
 
-class TapboxA extends StatefulWidget {
-  TapboxA({Key key}) : super(key: key);
-
+class ParentWidget extends StatefulWidget {
   @override
-  _TapboxAState createState() => _TapboxAState();
+  _ParentWidgetState createState() => _ParentWidgetState();
 }
 
-class _TapboxAState extends State<TapboxA> {
+class _ParentWidgetState extends State<ParentWidget> {
   bool _active = false;
 
-  void _handleTap() {
+  void _handleTapboxChanged(bool newValue) {
     setState(() {
-      _active = !_active;
+      _active = newValue;
     });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TapboxB(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+class TapboxB extends StatelessWidget {
+  TapboxB({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
   }
 
   Widget build(BuildContext context) {
@@ -43,14 +63,14 @@ class _TapboxAState extends State<TapboxA> {
       child: Container(
         child: Center(
           child: Text(
-            _active ? 'Active' : 'Inactive',
+            active ? 'Active' : 'Inactive',
             style: TextStyle(fontSize: 32.0, color: Colors.white),
           ),
         ),
         width: 200.0,
         height: 200.0,
         decoration: BoxDecoration(
-          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+          color: active ? Colors.lightGreen[700] : Colors.grey[600],
         ),
       ),
     );

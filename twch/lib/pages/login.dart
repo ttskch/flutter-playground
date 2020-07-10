74b781c3ee39206b7be5449237660c5b7db19565
Takashi Kanemoto <ttskch@gmail.com>
2020-07-10 16:21:15 +0900

    feat: 🎸 ログイン処理中はログインボタンを見せないように修正

--- twch/lib/pages/login.dart
+++ twch/lib/pages/login.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:twch/services/auth.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
+  @override
+  createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  bool _loading = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Login')),
       body: Center(
-        child: MaterialButton(
-          child: Text('Login with Twitter'),
-          color: Colors.blue,
-          textColor: Colors.white,
-          onPressed: () async {
-            await Auth().loginWithTwitter();
-            Navigator.of(context).pushReplacementNamed('/account-list');
-          },
-        ),
+        child: _loading
+            ? CircularProgressIndicator()
+            : MaterialButton(
+                child: Text('Login with Twitter'),
+                color: Colors.blue,
+                textColor: Colors.white,
+                onPressed: () async {
+                  setState(() => _loading = true);
+                  await Auth().loginWithTwitter();
+                  Navigator.of(context).pushReplacementNamed('/account-list');
+                },
+              ),
       ),
     );
   }

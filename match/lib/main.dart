5e486fc178c5fb52f648441883de6a5a97d3bc88
Takashi Kanemoto <ttskch@gmail.com>
2020-07-17 12:13:39 +0900

    feat: 🎸 match: ログイン画面の見た目を実装

--- match/lib/main.dart
+++ match/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:match/pages/login.dart';
 import 'package:match/services/auth.dart';
 
 void main() async {
@@ -13,52 +14,56 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'match',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('match'),
-        ),
-        floatingActionButton: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              margin: EdgeInsets.only(right: 12),
-              child: FloatingActionButton(
-                heroTag: 'login_with_email',
-                child: Icon(Icons.email),
-                onPressed: () async => print(
-                  await Auth.loginWithEmailAndPassword(
-                    email: 'test@test.com',
-                    password: 'testtest',
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(right: 12),
-              child: FloatingActionButton(
-                heroTag: 'login_with_twitter',
-                child: Text('Tw'),
-                onPressed: () async => print(
-                  await Auth.loginWithTwitter(),
-                ),
-              ),
-            ),
-            FloatingActionButton(
-              heroTag: 'add',
-              child: Icon(Icons.add),
-              onPressed: () {
-                Firestore.instance.collection('test').add({
-                  'content': 'test',
-                  'createdAt': FieldValue.serverTimestamp(),
-                });
-              },
-            ),
-          ],
-        ),
-        body: Center(
-          child: Text('match'),
-        ),
-      ),
+      initialRoute: '/',
+      routes: {
+        '/': (BuildContext context) => Login(),
+      },
+      // home: Scaffold(
+      //   appBar: AppBar(
+      //     title: Text('match'),
+      //   ),
+      //   floatingActionButton: Row(
+      //     mainAxisSize: MainAxisSize.min,
+      //     children: [
+      //       Container(
+      //         margin: EdgeInsets.only(right: 12),
+      //         child: FloatingActionButton(
+      //           heroTag: 'login_with_email',
+      //           child: Icon(Icons.email),
+      //           onPressed: () async => print(
+      //             await Auth.loginWithEmailAndPassword(
+      //               email: 'test@test.com',
+      //               password: 'testtest',
+      //             ),
+      //           ),
+      //         ),
+      //       ),
+      //       Container(
+      //         margin: EdgeInsets.only(right: 12),
+      //         child: FloatingActionButton(
+      //           heroTag: 'login_with_twitter',
+      //           child: Text('Tw'),
+      //           onPressed: () async => print(
+      //             await Auth.loginWithTwitter(),
+      //           ),
+      //         ),
+      //       ),
+      //       FloatingActionButton(
+      //         heroTag: 'add',
+      //         child: Icon(Icons.add),
+      //         onPressed: () {
+      //           Firestore.instance.collection('test').add({
+      //             'content': 'test',
+      //             'createdAt': FieldValue.serverTimestamp(),
+      //           });
+      //         },
+      //       ),
+      //     ],
+      //   ),
+      //   body: Center(
+      //     child: Text('match'),
+      //   ),
+      // ),
     );
   }
 }
---
+++ match/lib/pages/login.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:match/services/auth.dart';
+
+class Login extends StatefulWidget {
+  @override
+  createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  bool _loggingIn = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('ログイン')),
+      body: Center(
+        child: _loggingIn ? CircularProgressIndicator() : _buildLoginForm(),
+      ),
+    );
+  }
+
+  Widget _buildLoginForm() {
+    return Container(
+      padding: EdgeInsets.only(left: 16.0, right: 16.0),
+      child: ListView(
+        children: [
+          Form(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'メールアドレス',
+                  ),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'パスワード',
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 10.0),
+                  child: RaisedButton(
+                    child: Text('ログイン'),
+                    onPressed: _loginWithEmailAndPassword,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Center(
+            child: Container(
+              padding: EdgeInsets.only(top: 28.0, bottom: 28.0),
+              child: Text('または'),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              RaisedButton(
+                child: Text('Twitterでログイン'),
+                color: Colors.blue,
+                textColor: Colors.white,
+                onPressed: () async {
+                  setState(() => _loggingIn = true);
+                  if (await Auth.loginWithTwitter() != null) {
+                    // Navigator.of(context).pushReplacementNamed('/home');
+                  } else {
+                    setState(() => _loggingIn = false);
+                  }
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _loginWithEmailAndPassword() {}
+}

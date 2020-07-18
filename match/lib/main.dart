606a49a4c2b5485303f5ae090c3d9e39fa935922
Takashi Kanemoto <ttskch@gmail.com>
2020-07-18 10:51:00 +0900

    refactor: 💡 match: 不要なコードを削除

--- match/lib/main.dart
+++ match/lib/main.dart
@@ -1,9 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:match/pages/login.dart';
 import 'package:match/pages/signup.dart';
-import 'package:match/services/auth.dart';
 
 void main() async {
   await DotEnv().load('.env');
@@ -20,52 +18,6 @@ class MyApp extends StatelessWidget {
         '/': (BuildContext context) => Login(),
         '/signup': (BuildContext context) => Signup(),
       },
-      // home: Scaffold(
-      //   appBar: AppBar(
-      //     title: Text('match'),
-      //   ),
-      //   floatingActionButton: Row(
-      //     mainAxisSize: MainAxisSize.min,
-      //     children: [
-      //       Container(
-      //         margin: EdgeInsets.only(right: 12),
-      //         child: FloatingActionButton(
-      //           heroTag: 'login_with_email',
-      //           child: Icon(Icons.email),
-      //           onPressed: () async => print(
-      //             await Auth.loginWithEmailAndPassword(
-      //               email: 'test@test.com',
-      //               password: 'testtest',
-      //             ),
-      //           ),
-      //         ),
-      //       ),
-      //       Container(
-      //         margin: EdgeInsets.only(right: 12),
-      //         child: FloatingActionButton(
-      //           heroTag: 'login_with_twitter',
-      //           child: Text('Tw'),
-      //           onPressed: () async => print(
-      //             await Auth.loginWithTwitter(),
-      //           ),
-      //         ),
-      //       ),
-      //       FloatingActionButton(
-      //         heroTag: 'add',
-      //         child: Icon(Icons.add),
-      //         onPressed: () {
-      //           Firestore.instance.collection('test').add({
-      //             'content': 'test',
-      //             'createdAt': FieldValue.serverTimestamp(),
-      //           });
-      //         },
-      //       ),
-      //     ],
-      //   ),
-      //   body: Center(
-      //     child: Text('match'),
-      //   ),
-      // ),
     );
   }
 }
